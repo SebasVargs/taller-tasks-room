07340ab08973d5f41ff2cca70f648a66d9e3842b
AxelPriet <axeprieto@uniboyaca.edu.co>
2025-05-23 17:52:44 -0500

    navigation-compose

--- app/src/main/java/com/example/taller_moviles/MainActivity.kt
+++ app/src/main/java/com/example/taller_moviles/MainActivity.kt
@@ -11,37 +11,80 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.taller_moviles.database.viewModels.TaskViewModel
 import com.example.taller_moviles.ui.theme.Taller_MovilesTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val taskDb by lazy { TaskViewModel(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Taller_MovilesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavigation()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AppNavigation(){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            HomeScreen(navController)
+        }
+        composable("task") {
+            TaskScreen(navController)
+        }
+        composable("group") {
+            GroupScreen(navController)
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomeScreen(navController: NavController) {
+
+}
+
+@Composable
+fun TaskScreen(navController: NavController) {
+
+}
+
+@Composable
+fun GroupScreen(navController: NavController) {
+
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 640)
+@Composable
+fun HomeScreenPreview() {
     Taller_MovilesTheme {
-        Greeting("Android")
+        HomeScreen(rememberNavController())
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 640)
+@Composable
+fun TaskScreenPreview() {
+    Taller_MovilesTheme {
+        TaskScreen(rememberNavController())
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 640)
+@Composable
+fun GroupScreenPreview() {
+    Taller_MovilesTheme {
+        GroupScreen(rememberNavController())
     }
 }
